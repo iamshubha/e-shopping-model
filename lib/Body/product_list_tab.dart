b0f4e70b3ce621480fc664aafb92f32121aa34a2
shubhabanerjee <you@example.com>
2020-04-21 18:18:18 +0530

    firebase data added

--- lib/Body/product_list_tab.dart
+++ lib/Body/product_list_tab.dart
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/Body/model/app_state_model.dart';
@@ -38,13 +24,14 @@ class ProductListTab extends StatelessWidget {
                 delegate: SliverChildBuilderDelegate(
                   (context, index) {
                     if (index < products.length) {
-                      return ProductRowItem(//TODO: item from here//TODO: it is flat button for navigate here from the category repo
+                      return ProductRowItem(
+                        //TODO: item from here
+                        //TODO: it is flat button for navigate here from the category repo
                         index: index,
                         product: products[index],
                         lastItem: index == products.length - 1,
                       );
                     }
-
                     return null;
                   },
                 ),
